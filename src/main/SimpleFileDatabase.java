23ceb68c03a81b0a1da7cfaeef1c9490463bc74e
Salamy10 <samuel.buerkle@gmail.com>
2026-05-30 14:23:42 +0200

    TryingFiles successful

--- src/main/SimpleFileDatabase.java
+++ src/main/SimpleFileDatabase.java
@@ -1,16 +1,15 @@
 package main;
 
 import java.awt.EventQueue;
+import java.io.File;
 
 import javax.swing.JFrame;
 
 public class SimpleFileDatabase {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
+	private JFrame frame;	
+	
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,8 +35,9 @@ public class SimpleFileDatabase {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(400, 200, 1920/2, 1080/2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	
+	
 }
---
+++ src/main/TryingFiles.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.io.File;
+
+public class TryingFiles {
+	public static void main(String[] args) {
+		
+		File ordner = new File("C:\\Git\\LogShift\\LilDataBase");
+		File[] dateien = ordner.listFiles();
+		
+
+		for (File datei : dateien) {
+			System.out.print("→ " + datei.getName() + "\n");
+			
+			
+			if(datei.isDirectory()) {
+				
+				File unterordner = new File("C:\\Git\\LogShift\\LilDataBase\\" + datei.getName());
+				File[] unterdateien = unterordner.listFiles();
+				
+				for (File unterdatei : unterdateien) {
+					System.out.print(" ↪ " + unterdatei.getName() + "\n");
+				}
+			}
+		}
+		
+		
+	}
+		
+
+}
