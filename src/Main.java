2f5c1a486bf0dfcc5a66a065a14efaca4ee50a01
Salamy10 <samuel.buerkle@gmail.com>
2026-05-30 11:50:37 +0200

    fenster -> window

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main extends JPanel{
 	}
 	
 	public static void main(String[] args) {		
-		JFrame fenster = new JFrame("LilGame");
-        fenster.setSize(1920, 1080);
-        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenster.add(new Main());
-        fenster.setVisible(true);	
+		JFrame window = new JFrame("LilGame");
+		window.setSize(1920, 1080);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(new Main());
+        window.setVisible(true);	
 	}
 
 }
