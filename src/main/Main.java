813f42981c3b1ff5e50ad884840a985dd7fdc82d
Salamy10 <samuel.buerkle@gmail.com>
2026-07-13 21:52:24 +0200

    new admin "admin" adminPasswort

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 	    DataBaseManager db = new DataBaseManager();
-	    String hash = db.hashPassword("adminPasswort");
+	    String hash = db.hashPassword("admin");
 	    User admin = new User("admin", hash, "LilDataBase", "admin");
 	    db.addUser(admin);
 	    System.out.println("Admin erstellt!");
